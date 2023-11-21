57436d26762d0b7b9d4ba8ac9fb8650e9d544f7f
Ryan McCoy <ryanmccoy@Ryans-MBP-2.attlocal.net>
2023-11-21 15:43:34 -0600

    added and filtered through the list of sheetrock images to get only the files I can work with

--- Building_Supplies/data_extractor.py
+++ Building_Supplies/data_extractor.py
@@ -15,8 +15,28 @@ from PIL import Image
 data_dir = 'data' 
 image_exts = ['jpeg','jpg', 'bmp', 'png']
 
+count_of_images = 0
+
 for image_class in os.listdir(data_dir):
-    print(image_class)
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try:
+            # Check if the file exists
+            if os.path.exists(image_path):
+                # Open the image
+                with Image.open(image_path) as img:
+                    # Get the format (image type)
+                    img_format = img.format
+                    print(img_format.lower())
+                    if img_format.lower() not in image_exts: 
+                        print('Image not in ext list {}'.format(image_path))
+                        os.remove(image_path)
+            else:
+                print(f"Error: File not found - {image_path}")
+        except Exception as e:
+            print('Issue with image {}'.format(image_path))
+            os.remove(image_path)
+
 
 """for image_class in os.listdir(data_dir): 
     print(image_class)
