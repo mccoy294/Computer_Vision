4956adaec4e5b052738f74839dc40cdaa8478823
Ryan McCoy <ryanmccoy@Ryans-MBP-2.attlocal.net>
2023-11-21 14:33:43 -0600

    Iterate through the file folder ands and check for extensions that aren't in the values

--- Building_Supplies/data_extractor.py
+++ Building_Supplies/data_extractor.py
@@ -5,3 +5,25 @@ import os
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus: 
     tf.config.experimental.set_memory_growth(gpu, True)
+
+
+
+#Data extraction
+import cv2
+import imghdr
+
+data_dir = 'data' 
+image_exts = ['jpeg','jpg', 'bmp', 'png']
+
+for image_class in os.listdir(data_dir): 
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try: 
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in image_exts: 
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e: 
+            print('Issue with image {}'.format(image_path))
+            # os.remove(image_path)
