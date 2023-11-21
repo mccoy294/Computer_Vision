2d88dbd5dce977eb4c003486800c39f3521ce299
Ryan McCoy <ryanmccoy@Ryans-MBP-2.attlocal.net>
2023-11-21 15:04:08 -0600

    Finally got the os to list all the data in folders

--- Building_Supplies/data_extractor.py
+++ Building_Supplies/data_extractor.py
@@ -5,17 +5,20 @@ import os
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus: 
     tf.config.experimental.set_memory_growth(gpu, True)
-
+    print(gpu)
 
 
 #Data extraction
 import cv2
-import imghdr
+from PIL import Image
 
 data_dir = 'data' 
 image_exts = ['jpeg','jpg', 'bmp', 'png']
 
-for image_class in os.listdir(data_dir): 
+print(os.listdir(data_dir))
+
+"""for image_class in os.listdir(data_dir): 
+    print(image_class)
     for image in os.listdir(os.path.join(data_dir, image_class)):
         image_path = os.path.join(data_dir, image_class, image)
         try: 
@@ -26,4 +29,4 @@ for image_class in os.listdir(data_dir):
                 os.remove(image_path)
         except Exception as e: 
             print('Issue with image {}'.format(image_path))
-            # os.remove(image_path)
+            # os.remove(image_path)"""
